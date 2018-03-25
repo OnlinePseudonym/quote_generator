213da92ddba8125f2f81092385b39596de2ad587
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-03-25 12:31:36 -0700

    add app component

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <body>
+                <h1>Design Quote Generator</h1>
+            </body>
+        )
+    }
+}
